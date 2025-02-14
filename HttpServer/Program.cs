42bd776ed70e96245e3b4cd11303c18a2268c4b7
simon-s-99 <142667693+simon-s-99@users.noreply.github.com>
2025-02-14 12:39:34 +0100

    refactor: switch code template to "Empty Console" instead
    this gives us a runnable exe on build & also a startable Program->Main structure (you cannot run a a class library)

---
+++ HttpServer/Program.cs
@@ -0,0 +1,13 @@
+﻿// A basic implementation of a http-server
+
+using System;
+
+namespace HttpServer;
+
+internal class Program
+{
+	static void Main(string[] args)
+	{
+		Console.WriteLine("Hello, World!");
+	}
+}
