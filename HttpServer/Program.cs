690786dc0eaed95e55d5a68857497cf577200d7c
simon-s-99 <142667693+simon-s-99@users.noreply.github.com>
2025-03-31 19:30:41 +0200

    feat: curl to "/" returns 200 OK but other paths return 404

--- HttpServer/Program.cs
+++ HttpServer/Program.cs
@@ -1,34 +1,41 @@
-﻿// A basic implementation of a http-server
-
-using System.Text; // used for conversions to/from byte[]/string w/ encoding
-using System.Net;
+﻿using System.Net;
 using System.Net.Sockets;
+using System.Threading.Tasks;
+
+// A basic implementation of a http-server
 
 namespace HttpServer;
 
 public class Program
 {
-	public static void Main(string[] args)
-	{
-		IPAddress listenerAddress = IPAddress.Any;
-		int listenerPort = 3999;
+    public static async Task Main(string[] args)
+    {
+        IPAddress listenerAddress = IPAddress.Any;
+        int listenerPort = 3999;
 
-		TcpListener listener = new(listenerAddress, listenerPort);
-		listener.Start(); // start listening to incoming requests
+        TcpListener listener = new(listenerAddress, listenerPort);
+        listener.Start(); // start listening to incoming requests
 
-		// accept connection requests & store connection-socket in a variable
-		var socket = listener.AcceptSocket(); 
+        // accept connection requests & store connection-socket in a variable
+        var socket = listener.AcceptSocket();
 
-		// response message explanation:
-		// HTTP/1.1 = http version
-		// 200 = status code
-		// OK = reason phrase (optional)
-		// \r\n marks end of section (a http response consists of 3 parts: status, headers, response body)
-		// NO HEADERS (but we still end this required section with \r\n)
-		byte[] responseBuffer = Encoding.UTF8.GetBytes("HTTP/1.1 200 OK\r\n\r\n");
+        // receive request info from client
+        var requestBuffer = new byte[1024];
+        int requestBytes = socket.Receive(requestBuffer);
+        var requestLines = System.Text.Encoding.UTF8.GetString(requestBuffer).Split("\r\n"); // \r\n marks end of section
 
-		socket.Send(responseBuffer);
+        var firstLine = requestLines[0].Split(' ');
+        var (method, path, httpVersion) = (firstLine[0], firstLine[1], firstLine[2]);
 
-		//Console.ReadLine();
-	}
+        // response message explanation:
+        // HTTP/1.1 = http version
+        // 200 = status code
+        // OK = reason phrase (optional)
+        // [status text here]
+
+        string response = path == "/" ? $"{httpVersion} 200 OK\r\n\r\n"
+            : $"{httpVersion} 404 Not Found\r\n\r\n";
+
+        socket.Send(System.Text.Encoding.UTF8.GetBytes(response));
+    }
 }
