cc277440eb60b228e182176b23b835c6d7bb4b62
simon-s-99 <142667693+simon-s-99@users.noreply.github.com>
2025-04-25 15:20:34 +0200

    chore: refactor httpserver code for easier overview
    & logical grouping

---
+++ HttpServer/Server.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Sockets;
+using System.Net;
+
+namespace HttpServer
+{
+    internal class Server
+    {
+        internal static void HttpServer()
+        {
+            TcpListener server = new TcpListener(IPAddress.Any, 4221);
+            server.Start();
+
+            while (true)
+            {
+                var socket = server.AcceptSocket(); // wait for client
+                Task.Run(() => Helpers.HandleSocket(socket));
+            }
+        }
+    }
+}
